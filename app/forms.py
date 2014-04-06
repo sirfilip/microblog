5a0e87cac708a44404f575ab7cd81c1e695792eb
Filip Kostovski <github.sirfilip@gmail.com>
2014-04-06 16:07:10 +0200

    Add avatar and edit profile

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,14 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
-from wtforms.validators import InputRequired
+from wtforms import TextField, BooleanField, TextAreaField
+from wtforms.validators import InputRequired, Length
 
 
 
 class LoginForm(Form):
     openid = TextField('openid', validators=[InputRequired()])
     remember_me = BooleanField('remember_me', default=False)
+
+class EditForm(Form):
+    nickname = TextField('nickname', validators=[InputRequired()])
+    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
+
